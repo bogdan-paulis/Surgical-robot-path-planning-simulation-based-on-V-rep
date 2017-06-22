bbf71506e1618845d2025034f477741890ac3ae7
neves <miguel.neves@tum.de>
2017-06-22 15:03:58 +0200

    restructured file locations. added communication block to vrep simulink (not working yet)

---
+++ common functios/square.m
@@ -0,0 +1,9 @@
+inverseKin(0.03,0,0)
+pause(5);
+inverseKin(0,0.03,0)
+pause(5);
+inverseKin(-0.03,0,0)
+pause(5);
+inverseKin(0,-0.03,0)
+pause(5);
+
--- square.m
+++
@@ -1,9 +0,0 @@
-inverseKin(0.003,0,0)
-pause(1);
-inverseKin(0,0.003,0)
-pause(1);
-inverseKin(-0.003,0,0)
-pause(2);
-inverseKin(0,-0.003,0)
-pause(2);
-
