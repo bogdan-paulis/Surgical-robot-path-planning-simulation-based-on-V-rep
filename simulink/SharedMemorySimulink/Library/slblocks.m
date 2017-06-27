276e2ac17227144b857320767bff8d9f571e0df9
neves <miguel.neves@tum.de>
2017-06-27 17:01:35 +0200

    updated shared memory example to support newest library. added a library called vrep in simulink.

---
+++ simulink/SharedMemorySimulink/Library/slblocks.m
@@ -0,0 +1,12 @@
+function blkStruct = slblocks
+		% This function specifies that the library should appear
+		% in the Library Browser
+		% and be cached in the browser repository
+
+		Browser.Library = 'Vrep_SM';
+		% 'mylib' is the name of the library
+
+		Browser.Name = 'Vrep';
+		% 'My Library' is the library name that appears in the Library Browser
+
+		blkStruct.Browser = Browser; 
