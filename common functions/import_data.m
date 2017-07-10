1452aa3468dd01b45a53e9e3ee0fce597ed71b05
Phillip <p.swazinna@tum.de>
2017-07-10 18:06:23 +0200

    added capability to start drawing at arbitrary times

--- common functions/import_data.m
+++ common functions/import_data.m
@@ -1,18 +1,17 @@
 disp('loading data points for square');
 square = dlmread('square.txt', ' ');
-xs_square = [square(:,1) square(:,2)];
-ys_square = [square(:,1) square(:,3)];
-zs_square = [square(:,1) square(:,4)];
+xs_square = square(:,2);
+ys_square = square(:,3);
+zs_square = square(:,4);
 
 disp('loading data points for circle');
 circle = dlmread('circle.txt', ' ');
-xs_circle = [circle(:,1) circle(:,2)];
-ys_circle = [circle(:,1) circle(:,3)];
-zs_circle = [circle(:,1) circle(:,4)];
+xs_circle = circle(:,2);
+ys_circle = circle(:,3);
+zs_circle = circle(:,4);
 
 disp('loading data points for heart');
 heart = dlmread('heart.txt', ' ');
-xs_heart = [heart(:,1) heart(:,2)];
-ys_heart = [heart(:,1) heart(:,3)];
-zs_heart = [heart(:,1) heart(:,4)];
-
+xs_heart = heart(:,2);
+ys_heart = heart(:,3);
+zs_heart = heart(:,4);
