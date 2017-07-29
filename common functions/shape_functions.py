89f34166cfcf33e28da6dc0fd90823efdc45ffa1
Phillip <p.swazinna@tum.de>
2017-07-29 13:08:22 +0200

    added square

--- common functions/shape_functions.py
+++ common functions/shape_functions.py
@@ -1,95 +1,114 @@
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-# circle
-f = open('docs/circle.txt', 'w')
 
-step_size = 0.02
-radius = 1.0
-time = 0
-z_const = 0.0
-x_coor = []
-y_coor = []
-# draw each quarter
-# first
-for x in np.arange(start=0.0, stop=radius+step_size, step=step_size):
-    y = np.sqrt((radius**2)-(x**2))
-    f.write(str(time) + ' ' + str(x) + ' ' + str(y) + ' ' + str(z_const) + '\n')
-    x_coor.append(x)
-    y_coor.append(y)
-    time += 1
-# second
-for x in np.arange(start=radius-step_size, stop=0.0, step=-step_size):
-    y = np.sqrt((radius**2)-(x**2))
-    f.write(str(time) + ' ' + str(x) + ' ' + str(-y) + ' ' + str(z_const) + '\n')
-    x_coor.append(x)
-    y_coor.append(-y)
-    time += 1
-# third
-for x in np.arange(start=0.0, stop=radius+step_size, step=step_size):
-    y = np.sqrt((radius**2)-(x**2))
-    f.write(str(time) + ' ' + str(-x) + ' ' + str(-y) + ' ' + str(z_const) + '\n')
-    x_coor.append(-x)
-    y_coor.append(-y)
-    time += 1
-# fourth
-for x in np.arange(start=radius-step_size, stop=0.0, step=-step_size):
-    y = np.sqrt((radius**2)-(x**2))
-    f.write(str(time) + ' ' + str(-x) + ' ' + str(y) + ' ' + str(z_const) + '\n')
-    x_coor.append(-x)
-    y_coor.append(y)
-    time += 1
-f.write(str(time) + ' ' + str(0.0) + ' ' + str(radius) + ' ' + str(z_const))
-x_coor.append(0.0)
-y_coor.append(radius)
-f.close()
-
-plt.plot(x_coor, y_coor)
-plt.axis([-2,2,-2,2])
-plt.show()
+def double_coords(coords):
+    coords_new = []
+    for x, y, z in coords:
+        coords_new.append((x, y, z))
+        coords_new.append((x, y, z))
+    return coords_new
 
 
-# heart shape
-f = open('docs/heart.txt', 'w')
+def make_circle(double_points = True, draw=True, step_size = 0.02, radius = 1.0, z_const = 0.0):
+    # circle
+    f = open('../docs/circle.txt', 'w')
+    mw = csv.writer(f, delimiter=" ")
 
-x_coor = []
-y_coor = []
-time = 0
-# compute each quarter
-# first
-for x in np.arange(start=0.0, stop=1.0+step_size, step=step_size):
-    y = np.abs(x)+np.sqrt(1 - (x**2))
-    f.write(str(time) + ' ' + str(x) + ' ' + str(y) + ' ' + str(z_const) + '\n')
-    x_coor.append(x)
-    y_coor.append(y)
-    time += 1
-# second
-for x in np.arange(start=1.0, stop=0.0, step=-step_size):
-    y = np.abs(x)-np.sqrt(1 - (x**2))
-    f.write(str(time) + ' ' + str(x) + ' ' + str(y) + ' ' + str(z_const) + '\n')
-    x_coor.append(x)
-    y_coor.append(y)
-    time += 1
-# third
-for x in np.arange(start=0.0, stop=1.0+step_size, step=step_size):
-    y = np.abs(x)-np.sqrt(1 - (x**2))
-    f.write(str(time) + ' ' + str(-x) + ' ' + str(y) + ' ' + str(z_const) + '\n')
-    x_coor.append(-x)
-    y_coor.append(y)
-    time += 1
-# fourth
-for x in np.arange(start=1.0, stop=0.0, step=-step_size):
-    y = np.abs(x)+np.sqrt(1 - (x**2))
-    f.write(str(time) + ' ' + str(-x) + ' ' + str(y) + ' ' + str(z_const) + '\n')
-    x_coor.append(-x)
-    y_coor.append(y)
-    time += 1
-f.write(str(time) + ' ' + str(0.0) + ' ' + str(1.0) + ' ' + str(z_const))
-x_coor.append(0.0)
-y_coor.append(1.0)
-f.close()
+    # first quarter
+    coords1 = []
+    for x in np.arange(start=0.0, stop=radius+step_size, step=step_size):
+        y = np.sqrt((radius**2)-(x**2))
+        coords1.append((x,y,z_const))
+    # other quarters
+    coords2 = list(reversed([(x,-y,z) for (x,y,z) in coords1]))
+    coords3 = [(-x,-y,z) for (x,y,z) in coords1]
+    coords4 = list(reversed([(-x,y,z) for (x,y,z) in coords1]))
+    coords = coords1 + coords2 + coords3 + coords4
+
+    # double points
+    if double_points:
+        coords = double_coords(coords)
+
+    # write file
+    for i in range(len(coords)):
+        mw.writerow([i, coords[i][0], coords[i][1], coords[i][2]])
+    f.close()
+
+    # draw?
+    if draw:
+        plt.plot([x for (x,y,z) in coords], [y for (x,y,z) in coords])
+        plt.axis([-2,2,-2,2])
+        plt.show()
 
 
-plt.plot(x_coor, y_coor)
-plt.axis([-2,2,-2,2])
-plt.show()
+def make_heart(double_points = True, draw=True, step_size = 0.02, z_const = 0.0):
+    # heart shape
+    f = open('../docs/heart.txt', 'w')
+    mw = csv.writer(f, delimiter=" ")
+
+    # compute each quarter
+    # first
+    coords1 = []
+    for x in np.arange(start=0.0, stop=1.0+step_size, step=step_size):
+        y = np.abs(x)+np.sqrt(1 - (x**2))
+        coords1.append((x,y,z_const))
+    coords4 = list(reversed([(-x,y,z) for (x,y,z) in coords1])) # fourth
+    # third
+    coords3 = []
+    for x in np.arange(start=0.0, stop=1.0 + step_size, step=step_size):
+        y = np.abs(x) - np.sqrt(1 - (x ** 2))
+        coords3.append((-x,y,z_const))
+    coords2 = list(reversed([(-x,y,z) for (x,y,z) in coords3]))
+    coords = coords1 + coords2 + coords3 + coords4
+
+    # double points
+    if double_points:
+        coords = double_coords(coords)
+
+    # write file
+    for i in range(len(coords)):
+        mw.writerow([i, coords[i][0], coords[i][1], coords[i][2]])
+    f.close()
+
+    # draw?
+    if draw:
+        plt.plot([x for (x,y,z) in coords], [y for (x,y,z) in coords])
+        plt.axis([-2,2,-2,2])
+        plt.show()
+
+
+def make_quare(double_points = True, draw=True, step_size = 0.1, z_const = 0.0):
+    # square shape
+    f = open('../docs/heart.txt', 'w')
+    mw = csv.writer(f, delimiter=" ")
+
+    coords1 = [(x, 1.0, z_const) for x in np.arange(start=0.0, stop=1.0, step=step_size)]
+    coords2 = [(1.0, y, z_const) for y in np.arange(start=1.0, stop=-1.0 - step_size, step=-step_size)]
+    coords3 = list(reversed([(x,-y,z) for (x,y,z) in coords1]))
+    coords4 = list(reversed([(-x,-y,z) for (x,y,z) in coords1]))
+    coords5 = list(reversed([(-x,y,z) for (x,y,z) in coords2]))
+    coords6 = list(reversed([(-x,y,z) for (x,y,z) in coords1]))
+    coords = coords1 + coords2 + coords3 + coords4 + coords5 + coords6
+
+    # double points
+    if double_points:
+        coords = double_coords(coords)
+
+    # write file
+    for i in range(len(coords)):
+        mw.writerow([i, coords[i][0], coords[i][1], coords[i][2]])
+    f.close()
+
+    # draw?
+    if draw:
+        plt.plot([x for (x, y, z) in coords], [y for (x, y, z) in coords])
+        plt.axis([-2, 2, -2, 2])
+        plt.show()
+
+
+if __name__ == "__main__":
+    #make_circle()
+    #make_heart()
+    make_quare()
